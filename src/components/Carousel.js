b8d7e0be39b9fd20f97da694ed5ebcad77d6b32f
George Trapp <george@Georges-MacBook-Pro.local>
2022-03-23 14:20:51 -0700

    First commit

---
+++ src/components/Carousel.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { SlideContent } from './SlideContent';
+
+const Carousel = ({ delay, children }) => {
+
+  const [current, setCurrent ] = useState(0);
+  const length = children.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+    
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  if (!Array.isArray(children) || children.length <= 0) {
+    return null;
+    
+  }
+
+ 
+
+  return (
+    <div className="carousel">
+    
+      {SlideContent.map((slide, index) => {
+        return (
+          <div
+            className={index === current ? "slide active" : "slide"}
+            key={index}
+          >
+            {index === current && (
+              <span>{slide.content}</span>
+            )}
+          </div>
+          
+        );
+      })}
+      
+    <div className="buttons">
+      <button className="button-previous" onClick={prevSlide}> Previous </button>
+      <button className="button-next" onClick={nextSlide}> Next </button>
+    </div>
+  </div>
+  );
+};
+
+export default Carousel;
